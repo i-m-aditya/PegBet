55b34893303acd9fa806defd033985779ae60f8d
i-m-aditya <prblmslvr.aditya@gmail.com>
2023-07-29 17:33:22 +0530

    semifungible token and its test

--- src/NewToken.sol
+++ src/NewToken.sol
@@ -1,8 +1,8 @@
 
 
-contract NewToken is ERC1155 {
-    uint256 public constant GOLD = 0;
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
-        _mint(msg.sender, GOLD, 10**18, "");
-    }
-}
+// contract NewToken is ERC1155 {
+//     uint256 public constant GOLD = 0;
+//     constructor() ERC1155("https://game.example/api/item/{id}.json") {
+//         _mint(msg.sender, GOLD, 10**18, "");
+//     }
+// }
