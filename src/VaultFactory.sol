233609282fa8112587af91744b8f212246df023d
i-m-aditya <prblmslvr.aditya@gmail.com>
2023-08-04 12:50:34 +0530

    Test

--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -1,11 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
-
-contract VaultFactory {
-    function createVault() public returns (address) {
-        Vault vault = new Vault();
-        return address(vault);
-    }
-}
+contract VaultFactory {}
