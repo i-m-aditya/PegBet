4dc14d4a77406175ddc57e8863b69100b5c3bde6
i-m-aditya <prblmslvr.aditya@gmail.com>
2023-08-04 12:44:31 +0530

    Test

--- src/Controller.sol
+++ src/Controller.sol
@@ -3,18 +3,22 @@ pragma solidity ^0.8.13;
 
 import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// import {EACAggregatorProxy} from "chainlink/contracts/src/v0.8/EACAggregatorProxy.sol";
+
 contract Controller {
-    constructor() {
-        // priceFeed = AggregatorV2V3Interface(_priceFeed);
-    }
+    constructor() {}
 
     event Log(string message);
+    event Log2(uint80 message);
 
     function getLatestPrice(address assetAddress) public returns (uint256) {
         // address usdcAddress = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
         AggregatorV3Interface priceFeed = AggregatorV3Interface(assetAddress);
 
-        emit Log("getLatestPrice");
+        string memory description = priceFeed.description();
+
+        emit Log(description);
+        // emit Log("getLatestPrice");
         (, int256 price, , , ) = priceFeed.latestRoundData();
         return uint256(price);
     }
--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.13;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract VaultFactory {}
+contract VaultFactory {
+    function createVault() public returns (address) {
+        Vault vault = new Vault();
+        return address(vault);
+    }
+}
