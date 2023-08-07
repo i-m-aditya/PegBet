100896ee6b0dca179edb03030f1bfd5765a47917
i-m-aditya <prblmslvr.aditya@gmail.com>
2023-08-07 17:09:06 +0530

    Test

--- src/Pegbet.sol
+++ src/Pegbet.sol
@@ -9,4 +9,14 @@ contract Pegbet is ERC20 {
         string memory symbol,
         uint8 decimals
     ) ERC20(name, symbol, decimals) {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
+    receive() external payable {}
 }
